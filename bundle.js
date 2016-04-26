f0bcf87025b314b9ee30758b59d86d1666b757bf
Frank Finger <franklinfinger@gmail.com>
2016-04-26 14:18:23 -0400

    rebundled and updated routes

--- bundle.js
+++ bundle.js
@@ -116,12 +116,12 @@ angular
   .config(function($routeProvider) {
     $routeProvider
       .when('/jepapp',{
-        templateUrl: "../jepApp/templates/index.html",
+        templateUrl: "./jepApp/templates/index.html",
         controller: "HomeController as HomeCtrl",
         controllerAs: "HomeCtrl"
       })
       .when('/question',{
-        templateUrl: "../jepApp/templates/questions.html",
+        templateUrl: "./jepApp/templates/questions.html",
         controller: "QuestionController"
       })
       .otherwise({
